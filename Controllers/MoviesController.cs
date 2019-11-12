51eef6174abf5d175ac375349af59d4cf9de544e
sabljica <tarik.sabljica7@gmail.com>
2019-11-13 00:56:31 +0100

    Not working on this anymore

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+using WebApplication2.ViewModels;
+using System.Data;
+
+namespace WebApplication2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "ASDFGH" };
+            var customers = new List<Customer>
+            {
+                new Customer{Name="Customer 1"},
+                new Customer{Name="Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers=customers
+            };
+
+
+            return View(viewModel);
+        }
+
+       
+    }
+} 
